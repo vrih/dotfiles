beebe92fc6e5239f49e335a3f781e24038807ad1
Daniel Bowman <dan@danbowtell.co.uk>
2018-03-11 12:26:36 +0000

    Modernise and detokenise pagerduty checker

--- i3/i3blocks/pagerduty-rust/src/main.rs
+++ i3/i3blocks/pagerduty-rust/src/main.rs
@@ -1,66 +1,37 @@
 #[macro_use] extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
+extern crate futures;
+extern crate serde_json;
 
 use std::error::Error;
-use std::io::Read;
+//use std::io::Read;
 use std::process;
 use std::env;
 
 extern crate rustc_serialize;
 
-use rustc_serialize::json;
+//use serde_json;
+use serde_json::Value;
+ 
+use hyper::{Client, Request, Method};
+use hyper::header::{Accept, qitem};
+use hyper_tls::HttpsConnector;
 
-use hyper::Client;
-use hyper::header::{Headers, Accept, qitem};
-use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+use futures::{Future, Stream};
+
+use tokio_core::reactor::Core;
 
 header! { (Token, "Authorization") => [String]}
 
 const URL_ROOT: &'static str = "https://api.pagerduty.com/";
 
-fn make_call(url_path: String) -> Result<String, Box<Error+Send+Sync>>{
-    let client = Client::new();
-
-    let mut auth_head = Headers::new();
-
-    auth_head.set(Token(TOKEN.to_owned()));
-    auth_head.set(Accept(vec![qitem(Mime(TopLevel::Application,
-                                         SubLevel::Ext("vnd.pagerduty+json".to_owned()),
-                                         vec![(Attr::Ext("version".to_owned()),
-                                               Value::Ext("2".to_owned()))]))]));
-
-    let url = URL_ROOT.to_owned() + &url_path;
-
-    let mut res = try!(client.get(&url)
-        .headers(auth_head)
-        .send());
-
-    // if it's not a 200 then error out
-    match res.status{
-        hyper::status::StatusCode::Ok => {
-            let mut body: String = String::new();
-            try!(res.read_to_string(&mut body));
-            Ok(body)},
-        
-        _ => Err(From::from(format!("{}", res.status)))
-    }
-}
-
-fn incidents(){
-    let body: String;
-    
-    match make_call(String::from("incidents?statuses%5B%5D=triggered&statuses%5B%5D=acknowledged")){
-        Ok(bod) => body = bod,
-        Err(err) => {println!("{}", err);
-                     process::exit(1)}
-    }
-
+fn print_incidents(body: hyper::Chunk) -> Result<(), hyper::Error>{
     // deserialize json
-    let request_json = json::Json::from_str(&body).unwrap();
-
-    let response_obj = request_json.as_object().unwrap();
+    let request_json: Value = serde_json::from_slice(&body).unwrap();
 
     // unwrap is fine because there should always be incidents object
-    let incidents = response_obj.get("incidents").unwrap();
+    let incidents = &request_json["incidents"];
 
     // unwrap is fine because it s
     let incidents_obj = incidents.as_array().unwrap();
@@ -75,10 +46,11 @@ fn incidents(){
     let mut trig: i32 = 0;
     
     let statuses  = incidents_obj.iter()
-        .map(|incident| {match incident.as_object().unwrap().get("status").unwrap().as_string().unwrap(){
-            "acknowledged" => Status::ACKNOWLEDGED,
-            "triggered" => Status::TRIGGERED,
-            _ => Status::UNKNOWN}})
+        .map(|incident| {
+            match incident["status"].as_str().unwrap(){
+                "acknowledged" => Status::ACKNOWLEDGED,
+                "triggered" => Status::TRIGGERED,
+                _ => Status::UNKNOWN}})
         .collect::<Vec<_>>();
 
     for status in &statuses{
@@ -111,34 +83,92 @@ fn call_loop(){
     process::exit(33);
 }
 
-fn oncall(){
-    let body: String;
-    // TODO: change endpoint
-    match make_call(String::from("oncalls?time_zone=UTC&user_ids%5B%5D=PYHMPK3&escalation_policy_ids%5B%5D=P6OGX5C&schedule_ids%5B%5D=PQJ4RJ7")){
-        Ok(bod) => body = bod,
-        Err(err) => {println!("{}", err);
-                     process::exit(1)}
-    }
-    
-    // deserialize json
-    let request_json = json::Json::from_str(&body).unwrap();
-    let response_obj = request_json.as_object().unwrap();
+fn print_oncall(body: hyper::Chunk) -> Result<(), hyper::Error>{
+    let request_json: Value = serde_json::from_slice(&body).unwrap();
 
-    let oncalls = response_obj.get("oncalls").unwrap();
+    let oncalls = &request_json["oncalls"];
     let oncalls_array = oncalls.as_array().unwrap();
 
     if oncalls_array.len() > 0{
-        call_loop();
+         call_loop();
     }
+    Ok(())
 }
 
+fn make_call<F>(url_path: &str, mut f: F) -> Result<(), Box<Error+Send+Sync>>
+    where F: FnMut(hyper::Chunk) -> Result<(), hyper::Error>{
+    let mut core = Core::new()?;
+    
+    let handle = core.handle();
+
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle)?)
+        .build(&handle);
+    
+    let mut url: std::string::String = URL_ROOT.to_owned();
+    url.push_str(url_path);
+
+    let mut req = Request::new(Method::Get, url.parse()?);
+    let mut token = String::from("Token token=");
+    token = token + &env::var("PAGERDUTY_TOKEN").expect("Set PAGERDUTY_TOKEN env");
+    req.headers_mut().set(Token(token));
+    req.headers_mut().set(Accept(vec![qitem("application/vnd.pagerduty+json;version=2".parse().unwrap())]));
+    
+    let a = client.request(req).and_then(|res| {
+        println!("{:?}", res.status());
+        // if it's not a 200 then error out
+       // match res.status(){
+        //    hyper::StatusCode::Ok => {
+                res.body().concat2().and_then(move |body: hyper::Chunk| {
+                    futures::done(f(body))
+                })
+            
+            
+           // _ => Err(From::from(format!("{}", res.status)))
+        }
+    
+    );    
+    core.run(a)?;
+                                        Ok(())
+}
+    
+
+fn incidents(){
+    make_call("incidents?statuses%5B%5D=triggered&statuses%5B%5D=acknowledged", print_incidents).unwrap();
+    }
 
 
+fn oncall(){
+    // let body: String;            
+    // TODO: change endpoint
+    make_call("oncalls?time_zone=UTC&user_ids%5B%5D=PYHMPK3&escalation_policy_ids%5B%5D=P6OGX5C&schedule_ids%5B%5D=PQJ4RJ7", print_oncall).unwrap();
+    //     Ok(bod) => body = bod,
+    //     Err(err) => {println!("{}", err);
+    //                  process::exit(1)}
+    // }
+    
+    // // deserialize json
+    // let request_json = json::Json::from_str(&body).unwrap();
+    // let response_obj = request_json.as_object().unwrap();
+
+    // let oncalls = response_obj.get("oncalls").unwrap();
+    // let oncalls_array = oncalls.as_array().unwrap();
+
+    // if oncalls_array.len() > 0{
+    //     call_loop();
+    // }
+}
 
 fn main(){
     if let Ok(s) = env::var("BLOCK_BUTTON"){
         if s == "1"{
-            let _ = process::Command::new("xdg-open").arg("https://infectiousmedia.pagerduty.com/incidents").output().unwrap_or_else(|e| { panic!("failed to execute process: {}", e)} );
+            let _ = process::Command::new("termite").arg("--hold")
+                .arg("-e")
+                .arg("coffee /home/daniel/Dropbox/work_scripts/vrih-pagerduty/src/vrih-pagerduty.coffee")
+                .spawn()
+                .unwrap_or_else(|e| { panic!("failed to execute process: {}", e)});
+
+//            let _ = process::Command::new("xdg-open").arg("https://infectiousmedia.pagerduty.com/incidents").output().unwrap_or_else(|e| { panic!("failed to execute process: {}", e)} );
         }
     }
 
